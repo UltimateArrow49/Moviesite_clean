09abe86fd1d60e77b7f7b616bc719117e0224a27
UltimateArrow49 <75139557+UltimateArrow49@users.noreply.github.com>
2025-10-01 18:08:07 +0100

    Refine Vidking embed handling and playback state

--- series_detail.js
+++ series_detail.js
@@ -103,9 +103,6 @@ function buildPlayerUrl(seasonNumber, episodeNumber, title) {
     autoPlay: "true",
     color: "14ff9f",
   });
-  if (state.show && state.show.poster_path) {
-    params.set("poster", IMG_BASE.replace("w780", "w500") + state.show.poster_path);
-  }
   return `/player.html?${params.toString()}`;
 }
 
