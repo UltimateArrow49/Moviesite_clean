aecf9115ce6b381e33c8a82674e7e902cdf60c58
UltimateArrow49 <75139557+UltimateArrow49@users.noreply.github.com>
2025-10-03 00:16:10 +0100

    Update Propeller service worker snippet

--- sw.js
+++ sw.js
@@ -1,17 +1,6 @@
-const PROP_ZONE_IDS = [9961316, 9961315, 9961314, 9961313];
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-try {
-  importScripts(`https://propu.sh/pfe/current/service-worker.min.js?z=${PROP_ZONE_IDS.join(',')}`);
-} catch (error) {
-  if (self && self.console && typeof self.console.error === 'function') {
-    self.console.error('Propeller multi-tag service worker failed to load', error);
-  }
+self.options = {
+    "domain": "fenoofaussut.net",
+    "zoneId": 9961316
 }
+self.lary = ""
+importScripts('https://fenoofaussut.net/act/files/service-worker.min.js?r=sw')
